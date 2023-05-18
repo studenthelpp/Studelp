5f07d88c844c69bce64c4ded78ea98010c07d23c
VedanthVDev <vedanthvdev1996@gmail.com>
2023-05-18 20:26:12 +0100

    SP-12: Cypress integration
    Adding cypress to move from selenium

--- client/cypress/e2e/integration/step_definition/login/login.js
+++ client/cypress/e2e/integration/step_definition/login/login.js
@@ -1,23 +1,38 @@
-const {
-  Before,
-  Given,
-  When,
-  Then,
-  After,
-} = require("cypress-cucumber-preprocessor/steps");
+const { Given, When, Then } = require("cypress-cucumber-preprocessor/steps");
 
-Given("I am on the login page", async () => {
-  await cy.visit("http://localhost:3000/login");
+Given("the user is on home page", async () => {
+  await cy.visit("http://localhost:3000/");
 });
 
-When(
-  "I enter my valid credentials {string} and {string}",
-  async (username, password) => {
-    await cy.get("#email").type(username);
-    await cy.get("#password").type(username);
-  }
-);
+When("the user clicks the login button", async () => {
+  await cy.get("#login-link").click();
+});
 
-When("I click the login button", async () => {
+When("the user is on the login page", async () => {
+  await cy.get("#forgot-password").contains("Forgotten Password");
+});
+
+When("the user enters {string} and {string}", async (username, password) => {
+  await cy.get("#email").type(username);
+  await cy.get("#password").type(password);
+});
+
+When("the user clicks submit", async () => {
   await cy.get("#submit").click();
+});
+
+Then("the user should see the navigation bar", async () => {
+  await cy.get(".navigation-bar").contains("Home");
+  await cy.get(".navigation-bar").contains("All Users");
+  await cy.get(".navigation-bar").contains("My Profile");
+  await cy.get(".navigation-bar").contains("Settings");
+});
+
+Then("the user selects settings tab", async () => {
+  await cy.contains("Settings").click();
+});
+
+Then("the user logs out", async () => {
+  cy.wait(5000);
+  await cy.contains("Log Out").click();
 });
